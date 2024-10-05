ec3173ec51e82186f4fb424763fb21186c917324
RahMA79 <rahoma772004@gmail.com>
2024-10-05 20:06:18 +0300

    Create Auth Cubit

---
+++ lib/cubits/Auth_Cubit/auth_cubit.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+  Future<void> loginMethod(
+      {required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      emit(LoginSuccess());
+    } on FirebaseAuthException catch (e) {
+      // Stop loading animation
+
+      if (e.code == 'user-not-found') {
+        emit(LoginFailure(errMessage: 'user-not-found'));
+      } else if (e.code == 'wrong-password') {
+        emit(LoginFailure(errMessage: 'Wrong password provided for that user'));
+      } else {
+        emit(LoginFailure(errMessage: 'There was an error'));
+      }
+    }
+  }
+
+  Future<void> registerMethod(
+      {required String email, required String password}) async {
+    emit(RegisterLoading());
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      emit(RegisterSuccess());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        emit(LoginFailure(errMessage: 'The password provided is too weak.'));
+      } else if (e.code == 'email-already-in-use') {
+        emit(LoginFailure(
+            errMessage: 'The account already exists for that email.'));
+      } else {
+        emit(LoginFailure(errMessage: 'There was an error'));
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:chat_app/Authonitication/login_page.dart';
 import 'package:chat_app/Authonitication/register_page.dart';
 import 'package:chat_app/Screens/chat_screen.dart';
 import 'package:chat_app/Screens/splash_screen.dart';
+import 'package:chat_app/cubits/Auth_Cubit/auth_cubit.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 //import 'package:chat_app/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -20,15 +22,20 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        'RegisterPage': (context) => const RegisterPage(),
-        'LoginPage': (context) => const LoginPage(),
-        'SplashScreen': (context) => const SplashScreen(),
-        'ChatScreen': (context) => ChatScreen()
-      },
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'SplashScreen',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AuthCubit()),
+      ],
+      child: MaterialApp(
+        routes: {
+          'RegisterPage': (context) => const RegisterPage(),
+          'LoginPage': (context) => const LoginPage(),
+          'SplashScreen': (context) => const SplashScreen(),
+          'ChatScreen': (context) => ChatScreen()
+        },
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'SplashScreen',
+      ),
     );
   }
 }
